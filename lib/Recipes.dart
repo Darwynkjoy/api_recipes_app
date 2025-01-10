9c8bc9a15d1195f65d987b1c1a6d46a390f50b66
darwynkjoy <darwynkjoy@gmail.com>
2025-01-10 23:10:44 +0530

    daily push

--- lib/Recipes.dart
+++ lib/Recipes.dart
@@ -7,28 +7,47 @@ class Pagerecipes extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      backgroundColor: const Color.fromARGB(255, 234, 245, 255),
       appBar: AppBar(
         title: Text(recipes.name,style: TextStyle(fontSize: 25,color: Colors.white,fontWeight: FontWeight.bold),),
         backgroundColor: Colors.blue,
+        leading: IconButton(onPressed: (){
+          Navigator.pop(context);
+        }, icon: Icon(Icons.arrow_back_ios,color: Colors.white,)),
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network(recipes.image),
+            Center(
+              child: Container(
+                height: 380,
+                width: 380,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),
+                border: Border.all(width: 3,color: const Color.fromARGB(255, 0, 140, 255)),
+                image: DecorationImage(image: NetworkImage(recipes.image))),
+                ),
+            ),
             Text("Name:${recipes.name}",style: TextStyle(fontSize: 25,color: Colors.black,fontWeight: FontWeight.bold),),
-            SizedBox(height: 10,),
-            Text("Cusines:${recipes.cuisine}",style: TextStyle(fontSize: 22,color: Colors.black,fontWeight: FontWeight.bold),),
-            SizedBox(height: 10,),
-            Text("Ingredients:${recipes.ingredients}",style: TextStyle(fontSize: 17,color: Colors.black),),
-            SizedBox(height: 10,),
-            Text("Instructions:${recipes.instructions}",style: TextStyle(fontSize: 17,color: Colors.black,),),
-            SizedBox(height: 10,),
-            Text("Cooktime:${recipes.cookTimeMinutes} minutes.",style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.bold),),
+            Text("Cusines:${recipes.cuisine}",style: TextStyle(fontSize: 21,color: Colors.black,fontWeight: FontWeight.bold),),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Ingredients",style: TextStyle(fontSize: 21,color: Colors.black,fontWeight: FontWeight.bold),),
+                Text("${recipes.ingredients}",style: TextStyle(fontSize: 17,color: Colors.black),),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Instructions:",style: TextStyle(fontSize: 21,color: Colors.black,fontWeight: FontWeight.bold),),
+                Text("${recipes.instructions}",style: TextStyle(fontSize: 17,color: Colors.black,),),
+              ],
+            ),
+            Text("Cooktime: ${recipes.cookTimeMinutes} minutes.",style: TextStyle(fontSize: 21,color: Colors.black,fontWeight: FontWeight.bold),),
           ],
                     ),
         ),
--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -36,22 +36,26 @@ class _firstpageState extends State<Firstpage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Recipe App"),
+        title: Text("Recipe App",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),),
         centerTitle: true,
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.blue,
       ),
 
       body:_isloading?
-      const Center(
-        child: CircularProgressIndicator(),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: const Center(
+          child: CircularProgressIndicator(),
+        ),
       )
       :dataFromAPI == null?
       const Center(
         child: Text("Failed To Load Data"),
       )
 
-      :GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: 2,crossAxisSpacing: 2),
+      :GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: 1,crossAxisSpacing: 1,childAspectRatio: 0.7),
       itemCount: dataFromAPI!.recipes.length,
       itemBuilder: (context,index){
         final Recipe=dataFromAPI!.recipes[index];
@@ -59,34 +63,37 @@ class _firstpageState extends State<Firstpage>{
           onTap: (){
             Navigator.push(context, MaterialPageRoute(builder: (context)=>Pagerecipes(recipes: Recipe)));
           },
-          child: Container(
-            height: 300,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Colors.black,
-            width: 2),
-            boxShadow: [
-              BoxShadow(
-                color: const Color.fromARGB(255, 196, 196, 196),
-                blurRadius: 0,
-                spreadRadius: 1,
-              )
-            ]),
-            margin: EdgeInsets.all(8.0),
-            padding: EdgeInsets.all(8.0),
-            child: Column(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.network(Recipe.image,width: 100,height: 65,),
-              SizedBox(height: 10,),
-              Text(Recipe.name,style: TextStyle(fontSize: 10,),),
-
-              Row(
-                children: [
-                  SizedBox(width: 30,),
-                  Text(Recipe.cuisine),
-                  SizedBox(width: 5,)
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8),
+              height: 300,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),
+              border: Border.all(color: const Color.fromARGB(255, 0, 140, 255),
+              width: 2),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 234, 245, 255),
+                  blurRadius: 0,
+                  spreadRadius: 1,
+                )
+              ]),
+              child: Column(mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Container(
+                    height: 150,
+                    width: 170,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(image: NetworkImage(Recipe.image),fit: BoxFit.cover)),
+                    ),
+                ),
+                SizedBox(height: 10,),
+                Text(Recipe.name,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,),),
+                Text(Recipe.cuisine,style: TextStyle(fontSize: 20),),
               ],
-            )
-            ],
+              ),
             ),
           ),
         );
